d183f1c3ac687f2f8d15730521aeda5b8f311405
juliatherese <44023066+juliatherese@users.noreply.github.com>
2019-10-29 21:18:15 +0100

    Add files via upload

---
+++ twentynineteen-child/functions.php
@@ -0,0 +1,37 @@
+<?php
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+// BEGIN ENQUEUE PARENT ACTION
+// AUTO GENERATED - Do not modify or remove comment markers above or below:
+
+if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
+    function chld_thm_cfg_locale_css( $uri ){
+        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
+            $uri = get_template_directory_uri() . '/rtl.css';
+        return $uri;
+    }
+endif;
+add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
+
+if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
+    function chld_thm_cfg_parent_css() {
+        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array( 'gutenshop-owl-slider-default','gutenshop-owl-slider-theme','gutenshop-foundation' ) );
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
+
+// END ENQUEUE PARENT ACTION
+
+add_filter( 'menu_image_default_sizes', function($sizes){
+  
+  // remove the default 36x36 size
+  unset($sizes['menu-36x36']);
+  
+  // add a new size
+  $sizes['menu-5x5'] = array(5,5);
+  
+  // return $sizes (required)
+  return $sizes;
+  
+});
